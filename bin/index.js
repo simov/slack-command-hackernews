1d75c5e7930b7c412dbfd27f6e11feecad6ba135
simov <simeonvelichkov@gmail.com>
2017-03-03 10:55:22 +0200

    Return promise

--- bin/index.js
+++ bin/index.js
@@ -33,9 +33,7 @@ server.use(logger('dev'))
 server.use(bodyParser.urlencoded({extended: true}))
 
 server.use((req, res) => {
-  hackernews(req.body, (err, attachments) => {
-    res.json({attachments})
-  })
+  hackernews(req.body).then((attachments) => res.json({attachments}))
 })
 
 server.listen(argv.port, () => console.log('Oh Hi', argv.port, '!'))
