12aed40498d0e927fc057843d008b940097e2845
simov <simeonvelichkov@gmail.com>
2017-03-05 14:00:58 +0200

    Minor refactoring

--- bin/index.js
+++ bin/index.js
@@ -9,15 +9,13 @@ if (argv.help) {
   process.exit()
 }
 
-if (!argv.port) {
-  console.error('Specify --port number')
-  process.exit()
-}
+;['port', 'config'].forEach((key) => {
+  if (!argv[key]) {
+    console.error(`Specify --${key} number`)
+    process.exit()
+  }
+})
 
-if (!argv.config) {
-  console.error('Specify --config file')
-  process.exit()
-}
 
 var path = require('path')
 var env = process.env.NODE_ENV || argv.env || 'development'
@@ -26,14 +24,15 @@ var config = require(path.resolve(process.cwd(), argv.config))[env]
 var express = require('express')
 var bodyParser = require('body-parser')
 var logger = require('morgan')
-var hackernews = require('../lib/')(config)
+var hackernews = require('../')(config)
 
 var server = express()
 server.use(logger('dev'))
 server.use(bodyParser.urlencoded({extended: true}))
 
 server.use((req, res) => {
-  hackernews(req.body).then((attachments) => res.json({attachments}))
+  hackernews(req.body)
+    .then((attachments) => res.json({attachments}))
 })
 
 server.listen(argv.port, () => console.log('Oh Hi', argv.port, '!'))
